e422583b252a4714af330b3a80292516caf74354
Ji-Soo Kang <kangjisoo250@gmail.com>
2024-03-07 12:33:29 -0500

    First Comment

--- eclipse-workspace/Time/src/time/Time.java
+++ eclipse-workspace/Time/src/time/Time.java
@@ -23,8 +23,12 @@ public class Time
 {
 	public static void main(String[] args) 
 	{
+//		int totalSeconds = getTotalSeconds("10:10:10");
+//		System.out.println("Total Seconds = " + totalSeconds);
+		
 		int totalSeconds = getTotalSeconds("10:10:10");
-		System.out.println("Total Seconds = " + totalSeconds);
+		System.out.println("**GH Total Seconds = " + totalSeconds);
+		
 		
 		/*try
 		{
